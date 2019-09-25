8a93a9fd60232951fc5731b780c040ce6702be35
Nati <natan.kamusher@gmail.com>
2019-09-25 21:02:54 +0300

    getting started completed

--- app.js
+++ app.js
@@ -2,3 +2,29 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://nathank:windows10@ds359077.mlab.com:59077/tutorial-mng')
 
 var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("wer'e connected");
+});
+
+var kittySchema = new mongoose.Schema({
+  name: String
+})
+
+kittySchema.methods.speak = function () {
+  var greeting = this.name ? "Meow name is " + this.name : "I don't have a name;"
+  console.log(greeting)
+}
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var fluffy = new Kitten({
+  name: 'fluffy'
+});
+fluffy.speak();
+
+fluffy.save()
+  .then(function (fluffy) {
+    fluffy.speak();
+  })
+  .catch(err => console.error(err))
