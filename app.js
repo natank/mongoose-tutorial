edb3cbdbe1e981c05f0631099a6e3874bdd73556
Nati <natan.kamusher@gmail.com>
2019-09-26 16:37:06 +0300

    updated server addresse

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://nathank:windows10@ds359077.mlab.com:59077/tutorial-mng')
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost:27017/tutorial-mongs')
+//mongoose.connect('mongodb://nathank:windows10@ds359077.mlab.com:59077/tutorial-mng')
 
 var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("wer'e connected");
+});
