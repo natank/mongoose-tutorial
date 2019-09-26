c0a69348e0a816dcb69169c625e9ae017188ee35
Nati <natan.kamusher@gmail.com>
2019-09-26 16:22:51 +0300

    completed schemas section

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-mongoose.connect('mongodb://nathank:windows10@ds359077.mlab.com:59077/tutorial-mng')
+mongoose.connect('mongodb://localhost:27017/tutorial-mongs')
+//mongoose.connect('mongodb://nathank:windows10@ds359077.mlab.com:59077/tutorial-mng')
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -33,10 +34,65 @@ var Blog = mongoose.model('Blog', blogSchema);
 
 var animalSchema = new Schema({
   name: String,
-  type: String
+  type: String,
+  tags: {type: [String], index: true}
 })
+
+animalSchema.set('autoIndex', false)
+
 animalSchema.methods.findSimilarTypes = function (cb) {
   return this.model('Animal').find({
     type: this.type
   }, cb)
 }
+
+
+
+animalSchema.statics.findByName = function(name){
+  return this.find({name: new RegExp(name, 'i')});
+}
+
+animalSchema.static('findByBreed', function(breed){
+  return this.find({breed});
+})
+
+
+animalSchema.index({name: 1, type: -1})
+
+var Animal = mongoose.model('Animal', animalSchema);
+
+Animal.on('index', function(error){
+  //console.log(error)
+})
+
+/**Virtuals */
+
+var personSchema = new Schema({
+  name: {
+    first: String,
+    last: String
+  }
+})
+
+personSchema.virtual('fullName')
+  .get(function(){
+    return this.name.first + ' '  + this.name.last;
+  })
+  .set(function(v){
+    this.name.first = v.substr(0, v.indexOf(' '));
+    this.name.last = v.substr(v.indexOf(' ')+1)
+  });
+
+  
+
+
+
+var Person = mongoose.model('Person', personSchema);
+
+var axl = new Person({
+  name: {first: 'Axl', last: 'Rose'}
+})
+
+axl.fullName = 'William Rose'
+
+console.log(axl.fullName);
