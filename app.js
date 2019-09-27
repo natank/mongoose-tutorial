9df8abfa3254b144cf66dbe4c69e483d2088bece
Nati <natan.kamusher@gmail.com>
2019-09-27 08:59:00 +0300

    Completed the connections tutrial section

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-mongoose.connect('mongodb://localhost:27017/tutorial-mongs')
-//mongoose.connect('mongodb://nathank:windows10@ds359077.mlab.com:59077/tutorial-mng')
+
+var MyModel = mongoose.model('Test', new Schema({
+  name: String
+}, {
+  bufferCommands: false
+}));
+MyModel.findOne(function (error, result) {
+  if (error) {
+    console.log(`mongoose error : ${error}`);
+  } else {
+    console.log(`result: ${result}`);
+  }
+})
+
+setTimeout(function () {
+  mongoose.connect('mongodb://nathank:windows10@ds359077.mlab.com:59077/tutorial-mng')
+}, 600000)
+//mongoose.connect('mongodb://localhost:27017/tutorial-mongs')
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
